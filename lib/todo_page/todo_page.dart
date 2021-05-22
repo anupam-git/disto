6ea367851c712e1fe202573300dc4d3c673a4e86
Anupam Basak <anupam.basak27@gmail.com>
2021-05-23 01:15:49 +0530

    Create todo page

---
+++ lib/todo_page/todo_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  TodoPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _TodoPageState createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+    );
+  }
+}
