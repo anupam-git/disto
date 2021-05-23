ae9ce6d4ec4c42c9c20d2f8a307e2be440527d57
Anupam Basak <anupam.basak27@gmail.com>
2021-05-23 20:50:50 +0530

    Remove title state variable

--- lib/pages/todo_page/todo_page.dart
+++ lib/pages/todo_page/todo_page.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:todo/util/constants.dart';
 
 class TodoPage extends StatefulWidget {
-  TodoPage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _TodoPageState createState() => _TodoPageState();
 }
@@ -15,7 +11,7 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Todos"),
       ),
       body: Center(
         child: Container(
