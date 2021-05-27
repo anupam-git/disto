9552c307b1fb57d3021f9cefb2a477db1ffd5a70
Anupam Basak <anupam.basak27@gmail.com>
2021-05-27 23:56:05 +0530

    Rename project

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo',
+      title: 'Disto',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
